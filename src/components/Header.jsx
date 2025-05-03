4a2501fa2bf9bade4fef5a86225581323334d7fb
jimmijameslinux <jj885928@gmail.com>
2025-05-03 22:13:15 +0530

    First commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import circularart from '../assets/circularart.png'
 import circularart2 from '../assets/circularart2.png'
@@ -7,18 +7,41 @@ import logo from '../assets/logo.png'
 import '../components/Header.css'
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
         <>
-            <div className='header'>
-                <img src={circularart} alt="Logo" style={{ width: "80px", height: "160px" }} />
-                <div className='innerheader'>
-                    <img src={lineart} alt="Logo" style={{ width: "500px" }} />
-                    <img src={logo} alt="Logo" style={{ width: "124px", height: "86px" }} />
-                    <img src={lineart} alt="Logo" style={{ width: "500px" }} />
+            <div className="header">
+                <div className="side-art">
+                    <img src={circularart} alt="Art Left" className="responsive-img tall" />
                 </div>
-                <img src={circularart2} alt="Logo" style={{ width: "80px", height: "160px" }} />
+
+                <div className="innerheader">
+                    <img src={lineart} alt="Line Art Left" className="responsive-img line" />
+                    <img src={logo} alt="Logo" className="responsive-img logo" />
+                    <img src={lineart} alt="Line Art Right" className="responsive-img line" />
+                </div>
+
+                <div className="side-art">
+                    <img src={circularart2} alt="Art Right" className="responsive-img tall" />
+                </div>
+
+                {/* Hamburger icon */}
+                <div className="hamburger" onClick={toggleMenu}>
+                {
+                    menuOpen?
+                        <span>X</span>
+                        :
+                        <span>☰</span>
+                        }
+                </div>
             </div>
-            <Navbar />
+
+            <Navbar isOpen={menuOpen} />
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../components/Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({ isOpen }) => {
   return (
-    <nav className="navbar">
-        <ul className="nav-menu">
-            <Link to="/" className="nav-links">
-              HOME
-            </Link>
-            <Link to="/about" className="nav-links">
-              ABOUT US
-            </Link>
-            <Link to="/ourcollection" className="nav-links">
-                OUR COLLECTION
-            </Link>
-            <Link to="/gallery" className="nav-links">
-                GALLERY
-            </Link>
-            <Link to="/contact" className="nav-links">
-              CONTACT US
-            </Link>
-        </ul>
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <ul className="nav-menu">
+        <li><Link to="/" className="nav-links">HOME</Link></li>
+        <li><Link to="/about" className="nav-links">ABOUT US</Link></li>
+        <li><Link to="/ourcollection" className="nav-links">OUR COLLECTION</Link></li>
+        <li><Link to="/gallery" className="nav-links">GALLERY</Link></li>
+        <li><Link to="/contact" className="nav-links">CONTACT US</Link></li>
+      </ul>
     </nav>
   )
 }
