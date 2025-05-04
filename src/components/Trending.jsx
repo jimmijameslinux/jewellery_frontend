04eb05e7f430ca6f76f8b4652d9935d6b4c500be
jimmijameslinux <jj885928@gmail.com>
2025-05-05 00:22:13 +0530

    Home Page Responsive

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import lineart from '../assets/lineart.png'
 import '../components/Trending.css'
 import radiantjewellery from '../assets/radiantjewellery.png'
+import indianarttraditional from '../assets/indianarttraditional.png'
+import logobig from '../assets/logobig.png'
+import certificate from '../assets/certificate.png'
 
 const Trending = () => {
     return (
-        <div className='tradingtop' style={{  }}>
-            {/* <svg width="300" height="150" viewBox="0 0 300 150" xmlns="http://www.w3.org/2000/svg">
+        <>
+            <div className='trendingtop'>
+                {/* <svg width="300" height="150" viewBox="0 0 300 150" xmlns="http://www.w3.org/2000/svg">
                 <path d="
                             M 10 150
                             Q 100 90 150 10
@@ -15,98 +19,147 @@ const Trending = () => {
                     fill="#F6F1EB"/>
             </svg> */}
 
-            <div className="trending">
-                <img src={lineart} alt="Line Art" className="responsive-img line" />
-                <h1>TRENDING</h1>
-                <img src={lineart} alt="Line Art" className="responsive-img line" />
+                <div className="trending">
+                    <img src={lineart} alt="Line Art" className="responsive-img line" />
+                    <h1>TRENDING</h1>
+                    <img src={lineart} alt="Line Art" className="responsive-img line" />
+                </div>
+                <div className="trendingitems">
+                    {/* 1 */}
+                    <div className="trendingitem">
+                        <div className="itemimage">
+                            <img src={radiantjewellery} alt="radiantjewellery" />
+                        </div>
+                        <h3 className="itemtitle">Radiant Halo Studs</h3>
+                        <p className="itemdescription">Where brilliance meets elegance.</p>
+                        <div className="ratingwithstars">
+                            {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
+                            <div className="stars">⭐⭐⭐⭐⭐</div>
+                            {/* total reviews */}
+                            <div className="totalreviews">1000+ reviews</div>
+                        </div>
+                        <div className="price">
+                            <div className="crossedprice">Rs 5999</div>
+                            <div className="originalprice">Rs 4999</div>
+                        </div>
+                        {/* <div className="addtocartbtn"> */}
+                        <button className="addtocartbtn">Add to Cart</button>
+                        {/* </div> */}
+                    </div>
+                    {/* 2 */}
+                    <div className="trendingitem">
+                        <div className="itemimage">
+                            <img src={radiantjewellery} alt="radiantjewellery" />
+                        </div>
+                        <h3 className="itemtitle">Radiant Halo Studs</h3>
+                        <p className="itemdescription">Where brilliance meets elegance.</p>
+                        <div className="ratingwithstars">
+                            {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
+                            <div className="stars">⭐⭐⭐⭐⭐</div>
+                            {/* total reviews */}
+                            <div className="totalreviews">1000+ reviews</div>
+                        </div>
+                        <div className="price">
+                            <div className="crossedprice">Rs 5999</div>
+                            <div className="originalprice">Rs 4999</div>
+                        </div>
+                        {/* <div className="addtocartbtn"> */}
+                        <button className="addtocartbtn">Add to Cart</button>
+                        {/* </div> */}
+                    </div>
+                    {/* 3 */}
+                    <div className="trendingitem">
+                        <div className="itemimage">
+                            <img src={radiantjewellery} alt="radiantjewellery" />
+                        </div>
+                        <h3 className="itemtitle">Radiant Halo Studs</h3>
+                        <p className="itemdescription">Where brilliance meets elegance.</p>
+                        <div className="ratingwithstars">
+                            {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
+                            <div className="stars">⭐⭐⭐⭐⭐</div>
+                            {/* total reviews */}
+                            <div className="totalreviews">1000+ reviews</div>
+                        </div>
+                        <div className="price">
+                            <div className="crossedprice">Rs 5999</div>
+                            <div className="originalprice">Rs 4999</div>
+                        </div>
+                        {/* <div className="addtocartbtn"> */}
+                        <button className="addtocartbtn">Add to Cart</button>
+                        {/* </div> */}
+                    </div>
+                    {/* 4 */}
+                    <div className="trendingitem">
+                        <div className="itemimage">
+                            <img src={radiantjewellery} alt="radiantjewellery" />
+                        </div>
+                        <h3 className="itemtitle">Radiant Halo Studs</h3>
+                        <p className="itemdescription">Where brilliance meets elegance.</p>
+                        <div className="ratingwithstars">
+                            {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
+                            <div className="stars">⭐⭐⭐⭐⭐</div>
+                            {/* total reviews */}
+                            <div className="totalreviews">1000+ reviews</div>
+                        </div>
+                        <div className="price">
+                            <div className="crossedprice">Rs 5999</div>
+                            <div className="originalprice">Rs 4999</div>
+                        </div>
+                        {/* <div className="addtocartbtn"> */}
+                        <button className="addtocartbtn">Add to Cart</button>
+                        {/* </div> */}
+                    </div>
+                </div>
+
+
             </div>
-            <div className="trendingitems">
-                {/* 1 */}
-                <div className="trendingitem">
-                    <div className="itemimage">
-                        <img src={radiantjewellery} alt="radiantjewellery" />
-                    </div>
-                    <h3 className="itemtitle">Radiant Halo Studs</h3>
-                    <p className="itemdescription">Where brilliance meets elegance.</p>
-                    <div className="ratingwithstars">
-                        {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
-                        <div className="stars">⭐⭐⭐⭐⭐</div>
-                        {/* total reviews */}
-                        <div className="totalreviews">1000+ reviews</div>
-                    </div>
-                    <div className="price">
-                        <div className="crossedprice">Rs 5999</div>
-                        <div className="originalprice">Rs 4999</div>
-                    </div>
-                    {/* <div className="addtocartbtn"> */}
-                    <button className="addtocartbtn">Add to Cart</button>
-                    {/* </div> */}
-                </div>
-                {/* 2 */}
-                <div className="trendingitem">
-                    <div className="itemimage">
-                        <img src={radiantjewellery} alt="radiantjewellery" />
-                    </div>
-                    <h3 className="itemtitle">Radiant Halo Studs</h3>
-                    <p className="itemdescription">Where brilliance meets elegance.</p>
-                    <div className="ratingwithstars">
-                        {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
-                        <div className="stars">⭐⭐⭐⭐⭐</div>
-                        {/* total reviews */}
-                        <div className="totalreviews">1000+ reviews</div>
-                    </div>
-                    <div className="price">
-                        <div className="crossedprice">Rs 5999</div>
-                        <div className="originalprice">Rs 4999</div>
-                    </div>
-                    {/* <div className="addtocartbtn"> */}
-                    <button className="addtocartbtn">Add to Cart</button>
-                    {/* </div> */}
-                </div>
-                {/* 3 */}
-                <div className="trendingitem">
-                    <div className="itemimage">
-                        <img src={radiantjewellery} alt="radiantjewellery" />
-                    </div>
-                    <h3 className="itemtitle">Radiant Halo Studs</h3>
-                    <p className="itemdescription">Where brilliance meets elegance.</p>
-                    <div className="ratingwithstars">
-                        {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
-                        <div className="stars">⭐⭐⭐⭐⭐</div>
-                        {/* total reviews */}
-                        <div className="totalreviews">1000+ reviews</div>
-                    </div>
-                    <div className="price">
-                        <div className="crossedprice">Rs 5999</div>
-                        <div className="originalprice">Rs 4999</div>
-                    </div>
-                    {/* <div className="addtocartbtn"> */}
-                    <button className="addtocartbtn">Add to Cart</button>
-                    {/* </div> */}
-                </div>
-                {/* 4 */}
-                <div className="trendingitem">
-                    <div className="itemimage">
-                        <img src={radiantjewellery} alt="radiantjewellery" />
-                    </div>
-                    <h3 className="itemtitle">Radiant Halo Studs</h3>
-                    <p className="itemdescription">Where brilliance meets elegance.</p>
-                    <div className="ratingwithstars">
-                        {/* <img src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZpcnN0JTIwY29sb3J8ZW58MHx8fHwxNjg5NTQ1MjA5&ixlib=rb-4.0.3&q=80&w=400" alt="Item" className="reatimg" /> */}
-                        <div className="stars">⭐⭐⭐⭐⭐</div>
-                        {/* total reviews */}
-                        <div className="totalreviews">1000+ reviews</div>
-                    </div>
-                    <div className="price">
-                        <div className="crossedprice">Rs 5999</div>
-                        <div className="originalprice">Rs 4999</div>
-                    </div>
-                    {/* <div className="addtocartbtn"> */}
-                    <button className="addtocartbtn">Add to Cart</button>
-                    {/* </div> */}
+            <div className="trendinghorimg">
+                <hr className='trendinghor' />
+                <div className="artcombined">
+                    <img className='indianarttraditional' src={indianarttraditional} alt='indianarttraditionalimg' />
+                    <img className='indianarttraditional2' src={indianarttraditional} alt='indianarttraditionalimg2' />
                 </div>
             </div>
-        </div>
+
+            <div className="jewelimgpromises">
+                <div className="jewelimg">
+                    <img src={logobig} alt="logobig" />
+                    <p>Crafting Purity, Celebrating Prosperity</p>
+                </div>
+                <div className="promises">
+                    <h4>
+                        <span className='span1'>
+                        Saubhagya 
+                        </span>
+                        <span className='span2'>
+                        Promises
+                        </span>
+                        </h4>
+                    <div className="littlepromises">
+                        <div className="prom">
+                            <img src={certificate} alt="certificate" />
+                            <p>The Purity Gurantee</p>
+                        </div>
+
+                        <div className="prom">
+                            <img src={certificate} alt="certificate" />
+                            <p>The Purity Gurantee</p>
+                        </div>
+
+                        <div className="prom">
+                            <img src={certificate} alt="certificate" />
+                            <p>The Purity Gurantee</p>
+                        </div>
+
+                        <div className="prom">
+                            <img src={certificate} alt="certificate" />
+                            <p>The Purity Gurantee</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
